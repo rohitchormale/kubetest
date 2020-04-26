1d0d8fda8e09a3b998f5650f86ccdc34ed937f63
Rohit Chormale <rohitchormale@gmail.com>
2020-04-26 14:47:40 +0530

    Added sample flask app to simulate probeservice

---
+++ bin/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+- Toggle status codes from 500 to 200, for testing liveness and readiness probes
+
+
+- requirements
+Flask==1.1.1
+
+"""
+
+import json
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route('/liveness')
+def liveness():
+    data = {"msg": "Liveness Check"} 
+    return json.dumps(data), 500
+
+
+@app.route('/readiness')
+def readiness():
+    data = {"msg": "Readiness Check"} 
+    return json.dumps(data), 200
+
+
+if __name__ == '__main__':
+    app.run()
